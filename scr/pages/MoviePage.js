34a8ec437cd2acd958542a4d8f4a7ab3e851285c
ryntsevich <ryntsevich.oksana@gmail.com>
2020-09-11 14:12:41 +0300

    feat: page MoviePage

--- scr/pages/MoviePage.js
+++ scr/pages/MoviePage.js
@@ -1,41 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-// import Movie from '../containers/Movie/Movie';
-
-// const MoviePage = props => {
-//     return (
-//         <div>
-//             <h1>{props.movie ? props.movie.title : null}</h1>
-//             <Movie id={props.match.params.movieId} />
-//         </div>
-//     );
-// };
-
-// const mapStateToProps = state => {
-//     return {
-//         movie: state.movie.movie,
-//     };
-// };
-
-// export default connect(mapStateToProps)(MoviePage);
+import MovieInfo from '../components/MovieInfo/MovieInfo';
 
 class MoviePage extends React.PureComponent {
-          
-    render() {
-  
-      // раз написано <Route path="/client/:clid" component={Page_Client} />
-      // значит Page_Client получит то что в УРЛе после /client/ под именем props.match.params.clid в виде строки
-      let movieId=parseInt(this.props.match.params.movieId);
-  
-      let clientData=appData.clientsArr.find( c => c.id==clientId );
-  
-      return (
-        <MobileClientInfo
-          info={clientData}
-        />
-      );
-      
-    }
-  
+
+  render() {
+
+    let movieId = parseInt(this.props.match.params.movieId);
+    console.log(movieId);
+
+    let movieInfo = this.props.movies.find(movie => movie.id == movieId);
+    console.log(movieInfo)
+
+    return (
+      <MovieInfo movieInfo={movieInfo} />
+    );
+
   }
+}
+
+const mapStateToProps = state => {
+  return {
+    movies: state.movies.data
+  };
+};
+export default connect(mapStateToProps)(MoviePage);
+
+
